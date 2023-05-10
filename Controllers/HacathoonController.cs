dd12dfd0532c9c30dbc90be9df70e7a81f6afdde
Aleksei Evstratov <alekseie@dmdevelopment.ru>
2023-05-10 17:05:30 +0700

    "Пользоветли" и "Хакатоны" в админке

---
+++ Controllers/HacathoonController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Hacathoon_Master.Controllers
+{
+    public class HacathoonController : Controller
+    {
+        private readonly IConfiguration _configuration;
+
+        public HacathoonController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+    }
+}
